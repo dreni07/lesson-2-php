989d64f01ac54e2728d1c6ecfe28200cef5938de
Elton-777 <erositoni@yahoo.com>
2024-09-19 17:46:17 +0200

    Lesson 2

---
+++ string.php
@@ -0,0 +1,20 @@
+<?php
+ $first_string = 'Dren';
+ $second_string = 'Llazani';
+ $concatinated = $first_string.$second_string;
+ echo $concatinated;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+    $name = 'Dren';
+    echo "i love $name";
+    $first_num = 1;
+    $second_num = 5;
+    $result = $first_num + $second_num;
+    echo $result;    
+    echo $second_num - $first_num;
+    echo '<br>';
+    echo 18 % 3;
+?>
